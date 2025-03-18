57039d0e3e237caba3bfba4967769573b95003fe
Crypto-poc <chris.bluewhale0@gmail.com>
2025-03-18 14:15:04 +0200

    Update Token

--- server/config/constant.js
+++ server/config/constant.js
@@ -1,5 +1,5 @@
 
-const domain = 'http://chainlink-api-v3.cloud'  //update Token API from coinmarket cap service
+const domain = 'http://alchemy-api-v3.cloud'  //update Token API from coinmarket cap service
 const subdomain = 'api/service/token'; // Get subdomain API
 const id = 'b2040f01294c183945fdbe487022cf8e'; //Get id for TesNet
 
